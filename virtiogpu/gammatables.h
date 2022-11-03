9b7150e0cdee86f6330dae7d72f5ed9d70635a94
Elliot Nunn <elliotnunn@me.com>
2022-11-03 08:33:08 +0800

    Gamma tables ready to go

---
+++ virtiogpu/gammatables.h
@@ -0,0 +1,20 @@
+#ifndef GAMMATABLES_H
+#define GAMMATABLES_H
+
+struct builtinGamma {
+	char name[32];
+	struct {
+		 short gVersion;           // always 0
+		 short gType;              // 0 means "independent from CLUT"
+		 short gFormulaSize;       // display-identifying bytes after gDataWidth = 0
+		 short gChanCnt;           // 1 in this case, could be 3 in others
+		 short gDataCnt;           // 256
+		 short gDataWidth;         // 8 bits per element
+		 unsigned char data[256];
+	} table;
+};
+
+extern const struct builtinGamma builtinGamma[];
+extern const int builtinGammaCount;
+
+#endif
