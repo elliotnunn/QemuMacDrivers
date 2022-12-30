9d5dcc29b9434f3994058fecaad5fc099890079d
Elliot Nunn <elliotnunn@me.com>
2022-12-30 11:18:00 +0800

    Testbed for Virgl

---
+++ virtio/virgl.c
@@ -0,0 +1,7 @@
+#include "lprintf.h"
+
+#include "virgl.h"
+
+void VirglTest(void) {
+	lprintf("Here is virgl\n");
+}
---
+++ virtio/virgl.h
@@ -0,0 +1,16 @@
+// For now, this file is a terrible hack to tell device-gpu.c and virgl.c about each other
+
+#ifndef VIRGIL_H
+#define VIRGIL_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+// Implement in virgl.c
+void VirglTest(void);
+
+// Implement in device-gpu.c
+// Take a buffer and return the "type" (error value)
+uint32_t VirglSend(void *req, size_t req_size);
+
+#endif
