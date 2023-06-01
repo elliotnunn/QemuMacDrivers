6fb33ca1c067cd83da95fedb8039a57a5a4fe288
Elliot Nunn <elliotnunn@me.com>
2023-06-01 19:33:51 +0800

    New commands but they don't quite work

--- virtio/device-9p.c
+++ virtio/device-9p.c
@@ -22,7 +22,7 @@ enum {
 	Rversion = 101, // size[4] Rversion tag[2] msize[4] version[s]
 	Tauth = 102,    // size[4] Tauth tag[2] afid[4] uname[s] aname[s]
 	Rauth = 103,    // size[4] Rauth tag[2] aqid[13]
-	Tattach = 104,  // size[4] Tattach tag[2] fid[4] afid[4] uname[s] aname[s] ... unix addition n_uname[4]
+	Tattach = 104,  // size[4] Tattach tag[2] fid[4] afid[4] uname[s] aname[s] ...UNIX n_uname[4]
 	Rattach = 105,  // size[4] Rattach tag[2] qid[13]
 	Terror = 106,   // illegal
 	Rerror = 107,   // size[4] Rerror tag[2] ename[s]
@@ -32,7 +32,7 @@ enum {
 	Rwalk = 111,    // size[4] Rwalk tag[2] nwqid[2] nwqid*(wqid[13])
 	Topen = 112,    // size[4] Topen tag[2] fid[4] mode[1]
 	Ropen = 113,    // size[4] Ropen tag[2] qid[13] iounit[4]
-	Tcreate = 114,  // size[4] Tcreate tag[2] fid[4] name[s] perm[4] mode[1]
+	Tcreate = 114,  // size[4] Tcreate tag[2] fid[4] name[s] perm[4] mode[1] ...UNIX extension[s]
 	Rcreate = 115,  // size[4] Rcreate tag[2] qid[13] iounit[4]
 	Tread = 116,    // size[4] Tread tag[2] fid[4] offset[8] count[4]
 	Rread = 117,    // size[4] Rread tag[2] count[4] data[count]
@@ -52,6 +52,7 @@ enum {
 	MY_MSIZE = 128*1024,
 	NOTAG = 0,
 	ONLYTAG = 1,
+	NOFID = 0xffffffff,
 };
 
 struct qid {
@@ -77,15 +78,15 @@ struct qid {
    (P)[1] = (0x0000FF00 & (V)) >> 8, \
    (P)[2] = (0x00FF0000 & (V)) >> 16, \
    (P)[3] = (0xFF000000 & (V)) >> 24, (P) + 4)
-#define WRITE64BE(P, V)                        \
-  ((P)[0] = (0xFF00000000000000 & (V)) >> 070, \
-   (P)[1] = (0x00FF000000000000 & (V)) >> 060, \
-   (P)[2] = (0x0000FF0000000000 & (V)) >> 050, \
-   (P)[3] = (0x000000FF00000000 & (V)) >> 040, \
-   (P)[4] = (0x00000000FF000000 & (V)) >> 030, \
-   (P)[5] = (0x0000000000FF0000 & (V)) >> 020, \
-   (P)[6] = (0x000000000000FF00 & (V)) >> 010, \
-   (P)[7] = (0x00000000000000FF & (V)) >> 000, (P) + 8)
+#define WRITE64LE(P, V)                        \
+  ((P)[0] = (0x00000000000000FF & (V)) >> 000, \
+   (P)[1] = (0x000000000000FF00 & (V)) >> 010, \
+   (P)[2] = (0x0000000000FF0000 & (V)) >> 020, \
+   (P)[3] = (0x00000000FF000000 & (V)) >> 030, \
+   (P)[4] = (0x000000FF00000000 & (V)) >> 040, \
+   (P)[5] = (0x0000FF0000000000 & (V)) >> 050, \
+   (P)[6] = (0x00FF000000000000 & (V)) >> 060, \
+   (P)[7] = (0xFF00000000000000 & (V)) >> 070, (P) + 8)
 
 
 static OSStatus initialize(DriverInitInfo *info);
@@ -93,6 +94,10 @@ static OSStatus finalize(DriverFinalInfo *info);
 static bool Version(uint32_t tx_msize, char *tx_version, uint32_t *rx_msize, char *rx_version);
 static bool Auth(uint32_t afid, char *uname, char *aname, struct qid *qid);
 static bool Attach(uint32_t tx_fid, uint32_t tx_afid, char *tx_uname, char *tx_aname, struct qid *rx_qid);
+static bool Walk(uint32_t tx_fid, uint32_t tx_newfid, char *tx_name, struct qid *rx_qid);
+static bool Open(uint32_t tx_fid, uint8_t tx_mode, struct qid *rx_qid, uint32_t *rx_iounit);
+static bool C_reate(uint32_t tx_fid, char *tx_name, uint32_t tx_perm, uint8_t tx_mode, char *tx_extn, struct qid *rx_qid, uint32_t *rx_iounit);
+static bool Read(uint32_t tx_fid, uint64_t tx_offset, uint32_t count, void *rx_data, uint32_t *done_count);
 static void SetErr(char *msg);
 
 DriverDescription TheDriverDescription = {
@@ -106,6 +111,7 @@ DriverDescription TheDriverDescription = {
 	{{kServiceCategoryNdrvDriver, kNdrvTypeIsGeneric, {0x00, 0x10, 0x80, 0x00}}}} //v0.1
 };
 
+uint16_t bufcnt;
 uint32_t msize;
 void *lpage;
 uint32_t ppage;
@@ -176,7 +182,8 @@ static OSStatus initialize(DriverInitInfo *info) {
 	// Cannot go any further without touching virtqueues, which requires DRIVER_OK
 	VDriverOK();
 
-	if (2 != QInit(0, 2)) {
+	bufcnt = QInit(0, 256);
+	if (bufcnt <= 2) {
 		lprintf("9p: failed QInit()\n");
 		return paramErr;
 	}
@@ -195,12 +202,20 @@ static OSStatus initialize(DriverInitInfo *info) {
 	lprintf("9p: msize %d\n", (long)msize);
 
 	struct qid root = {0};
+	uint32_t rootfid = 99;
 
-	if (Attach(99, 0xffffffff /*afid=NOFID*/, "", "", &root)) {
+	if (Attach(rootfid, NOFID /*afid=NOFID*/, "", "", &root)) {
 		return paramErr;
 	}
 
 	lprintf("qid %d %x %08x%08x\n", root.type, root.version, (uint32_t)(root.path >> 32), (uint32_t)root.path);
+
+	char big[256] = {};
+	uint32_t succeeded = 0;
+
+	Read(123, 0, sizeof(big), big, &succeeded);
+
+	lprintf("got %d bytes:\n", succeeded);
 
 	return noErr;
 }
@@ -284,6 +299,215 @@ static bool Attach(uint32_t tx_fid, uint32_t tx_afid, char *tx_uname, char *tx_a
 		rx_qid->type = b[7];
 		rx_qid->version = READ32LE(b+7+1);
 		rx_qid->path = READ64LE(b+7+1+4);
+	}
+
+	return false;
+}
+
+// Convenient just to enforce "one path element"
+static bool Walk(uint32_t tx_fid, uint32_t tx_newfid, char *tx_name, struct qid *rx_qid) {
+	char *b = lpage;
+	size_t nlen = strlen(tx_name);
+	uint32_t size = 4+1+2+4+4+2+2+nlen;
+
+	WRITE32LE(b, size);
+	b[4] = Twalk;
+	WRITE16LE(b+4+1, ONLYTAG);
+	WRITE32LE(b+4+1+2, tx_fid);
+	WRITE32LE(b+4+1+2+4, tx_newfid);
+	WRITE16LE(b+4+1+2+4+4, 1); // nwname
+	WRITE16LE(b+4+1+2+4+4+2, nlen);
+	   memcpy(b+4+1+2+4+4+2+2, tx_name, nlen);
+
+	flag = false;
+	QSend(0, 1, 1, (uint32_t[2]){ppage, ppage + 2048}, (uint32_t[2]){size, 2048}, NULL);
+	QNotify(0);
+	while (!flag) {} // Change to WaitForInterrupt
+
+	b = lpage + 2048;
+
+	if (b[4] == Rerror) {
+		SetErr(b);
+		return true;
+	}
+
+	if (rx_qid != NULL) {
+		rx_qid->type = b[9];
+		rx_qid->version = READ32LE(b+9+1);
+		rx_qid->path = READ64LE(b+9+1+4);
+	}
+
+	return false;
+}
+
+static bool Open(uint32_t tx_fid, uint8_t tx_mode, struct qid *rx_qid, uint32_t *rx_iounit) {
+	char *b = lpage;
+	uint32_t size = 4+1+2+4+1;
+
+	WRITE32LE(b, size);
+	*(b+4) = Topen;
+	WRITE16LE(b+4+1, ONLYTAG);
+	WRITE32LE(b+4+1+2, tx_fid);
+	*(b+4+1+2+4) = tx_mode;
+
+	flag = false;
+	QSend(0, 1, 1, (uint32_t[2]){ppage, ppage + 2048}, (uint32_t[2]){size, 24}, NULL);
+	QNotify(0);
+	while (!flag) {} // Change to WaitForInterrupt
+
+	b = lpage + 2048;
+
+	if (b[4] == Rerror) {
+		SetErr(b);
+		return true;
+	}
+
+	if (rx_qid != NULL) {
+		rx_qid->type = *(b+7);
+		rx_qid->version = READ32LE(b+7+1);
+		rx_qid->path = READ64LE(b+7+1+4);
+	}
+
+	if (rx_iounit != NULL) {
+		*rx_iounit = READ32LE(b+20);
+	}
+
+	return false;
+}
+
+static bool C_reate(uint32_t tx_fid, char *tx_name, uint32_t tx_perm, uint8_t tx_mode, char *tx_extn, struct qid *rx_qid, uint32_t *rx_iounit) {
+	char *b = lpage;
+	size_t nlen = strlen(tx_name), elen = strlen(tx_extn);
+	uint32_t size = 4+1+2+4+2+nlen+4+1+2+elen;
+
+	WRITE32LE(b, size);
+	*(b+4) = Tcreate;
+	WRITE16LE(b+4+1, ONLYTAG);
+	WRITE32LE(b+4+1+2, tx_fid);
+	WRITE16LE(b+4+1+2+4, nlen);
+	memcpy(b+4+1+2+4+2, tx_name, nlen);
+	WRITE32LE(b+4+1+2+4+2+nlen, tx_perm);
+	*(b+4+1+2+4+2+nlen+4) = tx_mode;
+	WRITE16LE(b+4+1+2+4+2+nlen+4+1, elen);
+	memcpy(b+4+1+2+4+2+nlen+4+1+2, tx_extn, elen);
+
+	flag = false;
+	QSend(0, 1, 1, (uint32_t[2]){ppage, ppage + 2048}, (uint32_t[2]){size, 24}, NULL);
+	QNotify(0);
+	while (!flag) {} // Change to WaitForInterrupt
+
+	b = lpage + 2048;
+
+	if (b[4] == Rerror) {
+		SetErr(b);
+		return true;
+	}
+
+	if (rx_qid != NULL) {
+		rx_qid->type = *(b+7);
+		rx_qid->version = READ32LE(b+7+1);
+		rx_qid->path = READ64LE(b+7+1+4);
+	}
+
+	if (rx_iounit != NULL) {
+		*rx_iounit = READ32LE(b+20);
+	}
+
+	return false;
+}
+
+// Ugh! Much complexity to work around here (at least we don't scatter-gather).
+static bool Read(uint32_t tx_fid, uint64_t tx_offset, uint32_t count, void *rx_data, uint32_t *done_count) {
+	*done_count = 0;
+
+	int maxpages = bufcnt - 2;
+
+	// actually, limit to the number of virtio buffers we command, bufcnt-2
+	uint32_t physicals[256] = {ppage, ppage + 2048};
+	uint32_t sizes[256] = {23, 11};
+
+	struct IOPreparationTable prep = {
+		.options = kIOLogicalRanges,
+		.addressSpace = kCurrentAddressSpaceID,
+		.granularity = 0,
+		.firstPrepared = 0,
+		.mappingEntryCount = maxpages,
+		.logicalMapping = NULL,
+		.physicalMapping = (void *)(physicals + 2),
+		.rangeInfo = {.range = {.base = rx_data, .length = count}},
+	};
+
+	while (*done_count < count) {
+		// Lock down as much of the receive buffer as possible
+		// Bad arguments should never happen
+		if (PrepareMemoryForIO(&prep) != noErr) {
+			return true;
+		}
+
+		lprintf("Prepared %d of requested %d bytes\n", prep.lengthPrepared, count);
+		lprintf("state = %d\n", prep.state);
+
+		int nbufs = 2;
+		uint32_t acctfor = 0;
+		uint32_t *this = physicals + nbufs;
+		uint32_t *that = sizes + nbufs;
+		while (acctfor < prep.lengthPrepared) {
+			nbufs++;
+
+			if (*this % 4096) {
+				*that = 4096 - (*this % 4096);
+			} else {
+				*that = 4096;
+			}
+
+			if (*that > prep.lengthPrepared - acctfor) {
+				*that = prep.lengthPrepared - acctfor;
+			}
+
+			acctfor += *that;
+			this++;
+			that++;
+		}
+
+		lprintf("buf sizes:\n");
+		for (int i=0; i<nbufs; i++) {
+			lprintf("%08x/%d ", physicals[i], sizes[i]);
+		}
+		lprintf("\n");
+
+		char *b = lpage;
+		WRITE32LE(b, sizes[0]);
+		*(b+4) = Tread;
+		WRITE16LE(b+4+1, ONLYTAG);
+		WRITE32LE(b+4+1+2, tx_fid);
+		WRITE64LE(b+4+1+2+4, tx_offset);
+		WRITE32LE(b+4+1+2+4+8, prep.lengthPrepared);
+
+		lprintf("message:");
+		for (int i=0; i<sizes[0]; i++) {
+			lprintf(" %02x", b[i]);
+		}
+		lprintf("\n");
+
+		flag = false;
+		QSend(0, 1, nbufs-1, physicals, sizes, NULL);
+		QNotify(0);
+		while (!flag) {} // Change to WaitForInterrupt
+
+		b = lpage + 2048;
+
+		if (b[4] == Rerror) {
+			lprintf("%c%c%s\n", b[9], b[10], rx_data);
+			// slightly tricky job here...
+			SetErr(b);
+			return true;
+		}
+
+		uint32_t got = READ32LE(b + 7);
+		lprintf("Got %d bytes of the %d prepared\n", got, prep.lengthPrepared);
+
+		*done_count += got;
+		CheckpointIO(prep.preparationID, 0);
 	}
 
 	return false;
