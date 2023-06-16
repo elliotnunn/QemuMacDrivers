fd0137bfa1e24455d731f951cb86dd5628f40c55
Elliot Nunn <elliotnunn@me.com>
2023-06-16 13:21:24 +0800

    9P debug spam

--- virtio/rpc9p.c
+++ virtio/rpc9p.c
@@ -105,12 +105,17 @@ volatile bool flag;
 static uint32_t curreadfid = (uint32_t)NOFID;
 #define INVAL_READDIR() {curreadfid = (uint32_t)NOFID;}
 
+#define QIDF "0x%02x.%x.%x"
+#define QIDA(qid) qid.type, qid.version, (uint32_t)qid.path
+#define READQID(ptr) (struct Qid9){*(char *)(ptr), READ32LE((char *)(ptr)+1), READ64LE((char *)(ptr)+5)}
+
 static bool Version9(uint32_t msize, char *version, uint32_t *retmsize, char *retversion);
 static bool checkErr(char *msg);
 static void putSmlGetBig(void);
 static void putBigGetSml(void);
 static bool allocBigBuffer(uint16_t maxbufs);
 static int pageExtents(const uint32_t *pages, int npages, uint32_t *extbases, uint32_t *extsizes, int maxext);
+static const char *errname(uint32_t errno);
 
 bool Init9(uint16_t vioq, uint16_t viobuffers) {
 	if (allocBigBuffer(viobuffers)) {
@@ -150,9 +155,14 @@ static bool Version9(uint32_t msize, char *version, uint32_t *retmsize, char *re
 
 	putSmlGetBig();
 
+	lprintf("Tversion(msize=%lu ver=%s)", msize, version);
+
 	if (checkErr(bigBuf)) {
 		return true;
 	}
+
+	lprintf(" -> (msize=%lu ver=%.*s)\n",
+		READ32LE(bigBuf+7), READ16LE(bigBuf+11), bigBuf+13);
 
 	if (retmsize != NULL) {
 		*retmsize = READ32LE(bigBuf+7);
@@ -187,17 +197,19 @@ bool Attach9(uint32_t fid, uint32_t afid, char *uname, char *aname, struct Qid9 
 	memcpy(smlBuf+4+1+2+4+4+2+ulen+2, aname, alen);
 	WRITE32LE(smlBuf+4+1+2+4+4+2+ulen+2+alen, 0); // numeric n_uname
 
+	lprintf("Tattach(fid=%lu afid=%lu uname=%s aname=%s)",
+		fid, afid, uname, aname);
+
 	putSmlGetBig();
 
 	if (checkErr(bigBuf)) {
 		return true;
 	}
 
-	if (retqid != NULL) {
-		retqid->type = bigBuf[7];
-		retqid->version = READ32LE(bigBuf+7+1);
-		retqid->path = READ64LE(bigBuf+7+1+4);
-	}
+	struct Qid9 qid = READQID(bigBuf+7);
+	lprintf(" -> (qid="QIDF")\n", QIDA(qid));
+
+	if (retqid != NULL) *retqid = qid;
 
 	return false;
 }
@@ -208,6 +220,8 @@ bool Walk9(uint32_t fid, uint32_t newfid, uint16_t nwname, const char **name, ui
 
 	INVAL_READDIR();
 
+	lprintf("Twalk(fid=%lu nfid=%lu wname=\"", fid, newfid);
+
 	*(bigBuf+4) = Twalk;
 	WRITE16LE(bigBuf+4+1, ONLYTAG);
 	WRITE32LE(bigBuf+4+1+2, fid);
@@ -216,6 +230,7 @@ bool Walk9(uint32_t fid, uint32_t newfid, uint16_t nwname, const char **name, ui
 
 	uint32_t size = 4+1+2+4+4+2;
 	for (uint16_t i=0; i<nwname; i++) {
+		lprintf("/%s", *name);
 		uint16_t len = strlen(*name);
 		WRITE16LE(bigBuf+size, len);
 		size += 2;
@@ -226,6 +241,8 @@ bool Walk9(uint32_t fid, uint32_t newfid, uint16_t nwname, const char **name, ui
 
 	WRITE32LE(bigBuf, size);
 
+	lprintf("\")");
+
 	putBigGetSml();
 
 	if (checkErr(smlBuf)) {
@@ -235,14 +252,13 @@ bool Walk9(uint32_t fid, uint32_t newfid, uint16_t nwname, const char **name, ui
 
 	uint16_t got = READ16LE(smlBuf+7);
 
+	lprintf(" -> (%u/%u)\n", got, nwname);
+
 	if (retnwqid) *retnwqid=got;
 
 	if (retqid != NULL) {
 		for (uint16_t i=0; i<got; i++) {
-			char *thisqid = smlBuf+9+13*i;
-			retqid[i].type = *thisqid;
-			retqid[i].version = READ32LE(thisqid+1);
-			retqid[i].path = READ64LE(thisqid+1+4);
+			retqid[i] = READQID(smlBuf+9+13*i);
 		}
 	}
 
@@ -263,21 +279,21 @@ bool Lopen9(uint32_t fid, uint32_t flags, struct Qid9 *retqid, uint32_t *retioun
 	WRITE32LE(smlBuf+4+1+2, fid);
 	WRITE32LE(smlBuf+4+1+2+4, flags);
 
+	lprintf("Tlopen(fid=%lu flags=%lu)", fid, flags);
+
 	putSmlGetBig();
 
 	if (checkErr(bigBuf)) {
 		return true;
 	}
 
-	if (retqid != NULL) {
-		retqid->type = *(bigBuf+4+1+2);
-		retqid->version = READ32LE(bigBuf+4+1+2+1);
-		retqid->path = READ64LE(bigBuf+4+1+2+1+4);
-	}
+	struct Qid9 qid = READQID(bigBuf+4+1+2);
+	uint32_t iounit = READ32LE(bigBuf+4+1+2+13);
 
-	if (retiounit != NULL) {
-		*retiounit = READ32LE(bigBuf+4+1+2+13);
-	}
+	lprintf(" -> (qid="QIDF" iounit=%lu)\n", QIDA(qid), iounit);
+
+	if (retqid != NULL) *retqid = qid;
+	if (retiounit != NULL) *retiounit = iounit;
 }
 
 // -1=err, 0=ok, 1=eof
@@ -306,6 +322,8 @@ char Readdir9(uint32_t fid, struct Qid9 *retqid, char *rettype, char retname[512
 		WRITE64LE(smlBuf+4+1+2+4, requestoffset);
 		WRITE32LE(smlBuf+4+1+2+4+8, Max9);
 
+		lprintf("Treaddir(fid=%lu)", fid);
+
 		putSmlGetBig();
 
 		if (checkErr(bigBuf)) {
@@ -315,32 +333,30 @@ char Readdir9(uint32_t fid, struct Qid9 *retqid, char *rettype, char retname[512
 		cursor = 0;
 		count = READ32LE(bigBuf+7);
 
+		lprintf(" -> (count=%lu)\n", count);
 
-		if (count==0) return 1; // eof
+		if (count==0) {
+			return 1; // eof
+		}
 	}
 
 	char *entry = bigBuf+11+cursor;
 
+	struct Qid9 qid = READQID(entry);
 	requestoffset = READ64LE(entry+13);
+	unsigned char type = *(entry+13+8);
+	uint16_t nlen = READ16LE(entry+13+8+1);
 
-	if (retqid != NULL) {
-		retqid->type = *(entry);
-		retqid->version = READ32LE(entry+1);
-		retqid->path = READ64LE(entry+1+4);
-	}
+	cursor += 13+8+1+2+nlen;
 
-	if (rettype != NULL) {
-		*rettype = *(entry+13+8);
-	}
+	if (retqid != NULL) *retqid = qid;
+	if (rettype != NULL) *rettype = type;
 
 	if (retname != NULL) {
-		uint16_t nlen = READ16LE(entry+13+8+1);
 		if (nlen > 511) nlen = 511;
 		memcpy(retname, entry+13+8+1+2, nlen);
 		retname[nlen] = 0;
 	}
-
-	cursor += 13+8+1+2+READ16LE(entry+13+8+1);
 
 	return 0;
 }
@@ -365,6 +381,8 @@ bool Getattr9(uint32_t fid, struct Qid9 *retqid, uint64_t *retsize, uint64_t *re
 	WRITE32LE(smlBuf+4+1+2, fid);
 	WRITE64LE(smlBuf+4+1+2+4, 0x200 /*size*/ | 0x80 /*ctime*/);
 
+	lprintf("Tgetattr(fid=%lu)", fid);
+
 	putSmlGetBig();
 
 	if (checkErr(bigBuf)) {
@@ -373,19 +391,15 @@ bool Getattr9(uint32_t fid, struct Qid9 *retqid, uint64_t *retsize, uint64_t *re
 
 	// Skip checking the valid-mask
 
-	if (retqid != NULL) {
-		retqid->type = bigBuf[15];
-		retqid->version = READ32LE(bigBuf+15+1);
-		retqid->path = READ64LE(bigBuf+15+1+4);
-	}
+	struct Qid9 qid = READQID(bigBuf+15);
+	uint64_t filesize = READ64LE(bigBuf+56);
+	uint64_t time = READ64LE(bigBuf+112);
 
-	if (retsize != NULL) {
-		*retsize = READ64LE(bigBuf+56);
-	}
+	lprintf(" -> (qid="QIDF" size=%lu time=%lu)\n", QIDA(qid), (uint32_t)filesize, (uint32_t)time);
 
-	if (rettime != NULL) {
-		*rettime = READ64LE(bigBuf+112);
-	}
+	if (retqid != NULL) *retqid = qid;
+	if (retsize != NULL) *retsize = filesize;
+	if (rettime != NULL) *rettime = time;
 
 	return false;
 }
@@ -403,9 +417,13 @@ bool Clunk9(uint32_t fid) {
 	WRITE16LE(smlBuf+4+1, ONLYTAG);
 	WRITE32LE(smlBuf+4+1+2, fid);
 
+	lprintf("Tclunk(fid=%lu)", fid);
+
 	putSmlGetBig();
 
-	return checkErr(bigBuf);
+	if (checkErr(bigBuf)) return true;
+
+	lprintf(" -> (ok)\n");
 }
 
 // You can leave the data pointer empty, and peruse bigBuf, if you prefer
@@ -451,7 +469,8 @@ static bool checkErr(char *msg) {
 	}
 
 	Err9 = READ32LE(msg+4+1+2);
-	lprintf(".virtio9p error: %d\n", Err9);
+
+	lprintf(" -> Rerror(%lu %s)\n", Err9, errname(Err9));
 	return true;
 }
 
@@ -568,6 +587,133 @@ static int pageExtents(const uint32_t *pages, int npages, uint32_t *extbases, ui
 		}
 	}
 	return extcnt;
+}
+
+static const char *errname(uint32_t errno) {
+	switch (errno) {
+		case 1: return "EPERM";
+		case 2: return "ENOENT";
+		case 3: return "ESRCH";
+		case 4: return "EINTR";
+		case 5: return "EIO";
+		case 6: return "ENXIO";
+		case 7: return "E2BIG";
+		case 8: return "ENOEXEC";
+		case 9: return "EBADF";
+		case 10: return "ECHILD";
+		case 11: return "EAGAIN";
+		case 12: return "ENOMEM";
+		case 13: return "EACCES";
+		case 14: return "EFAULT";
+		case 15: return "ENOTBLK";
+		case 16: return "EBUSY";
+		case 17: return "EEXIST";
+		case 18: return "EXDEV";
+		case 19: return "ENODEV";
+		case 20: return "ENOTDIR";
+		case 21: return "EISDIR";
+		case 22: return "EINVAL";
+		case 23: return "ENFILE";
+		case 24: return "EMFILE";
+		case 25: return "ENOTTY";
+		case 26: return "ETXTBSY";
+		case 27: return "EFBIG";
+		case 28: return "ENOSPC";
+		case 29: return "ESPIPE";
+		case 30: return "EROFS";
+		case 31: return "EMLINK";
+		case 32: return "EPIPE";
+		case 33: return "EDOM";
+		case 34: return "ERANGE";
+		case 35: return "EDEADLK";
+		case 36: return "ENAMETOOLONG";
+		case 37: return "ENOLCK";
+		case 38: return "ENOSYS";
+		case 39: return "ENOTEMPTY";
+		case 40: return "ELOOP";
+		case 41: return "EWOULDBLOCK";
+		case 42: return "ENOMSG";
+		case 43: return "EIDRM";
+		case 44: return "ECHRNG";
+		case 45: return "EL2NSYNC";
+		case 46: return "EL3HLT";
+		case 47: return "EL3RST";
+		case 48: return "ELNRNG";
+		case 49: return "EUNATCH";
+		case 50: return "ENOCSI";
+		case 51: return "EL2HLT";
+		case 52: return "EBADE";
+		case 53: return "EBADR";
+		case 54: return "EXFULL";
+		case 55: return "ENOANO";
+		case 56: return "EBADRQC";
+		case 57: return "EBADSLT";
+		case 58: return "EDEADLOCK";
+		case 59: return "EBFONT";
+		case 60: return "ENOSTR";
+		case 61: return "ENODATA";
+		case 62: return "ETIME";
+		case 63: return "ENOSR";
+		case 64: return "ENONET";
+		case 65: return "ENOPKG";
+		case 66: return "EREMOTE";
+		case 67: return "ENOLINK";
+		case 68: return "EADV";
+		case 69: return "ESRMNT";
+		case 70: return "ECOMM";
+		case 71: return "EPROTO";
+		case 72: return "EMULTIHOP";
+		case 73: return "EDOTDOT";
+		case 74: return "EBADMSG";
+		case 75: return "EOVERFLOW";
+		case 76: return "ENOTUNIQ";
+		case 77: return "EBADFD";
+		case 78: return "EREMCHG";
+		case 79: return "ELIBACC";
+		case 80: return "ELIBBAD";
+		case 81: return "ELIBSCN";
+		case 82: return "ELIBMAX";
+		case 83: return "ELIBEXEC";
+		case 84: return "EILSEQ";
+		case 85: return "ERESTART";
+		case 86: return "ESTRPIPE";
+		case 87: return "EUSERS";
+		case 88: return "ENOTSOCK";
+		case 89: return "EDESTADDRREQ";
+		case 90: return "EMSGSIZE";
+		case 91: return "EPROTOTYPE";
+		case 92: return "ENOPROTOOPT";
+		case 93: return "EPROTONOSUPPORT";
+		case 94: return "ESOCKTNOSUPPORT";
+		case 95: return "EOPNOTSUPP";
+		case 96: return "EPFNOSUPPORT";
+		case 97: return "EAFNOSUPPORT";
+		case 98: return "EADDRINUSE";
+		case 99: return "EADDRNOTAVAIL";
+		case 100: return "ENETDOWN";
+		case 101: return "ENETUNREACH";
+		case 102: return "ENETRESET";
+		case 103: return "ECONNABORTED";
+		case 104: return "ECONNRESET";
+		case 105: return "ENOBUFS";
+		case 106: return "EISCONN";
+		case 107: return "ENOTCONN";
+		case 108: return "ESHUTDOWN";
+		case 109: return "ETOOMANYREFS";
+		case 110: return "ETIMEDOUT";
+		case 111: return "ECONNREFUSED";
+		case 112: return "EHOSTDOWN";
+		case 113: return "EHOSTUNREACH";
+		case 114: return "EALREADY";
+		case 115: return "EINPROGRESS";
+		case 116: return "ESTALE";
+		case 117: return "EUCLEAN";
+		case 118: return "ENOTNAM";
+		case 119: return "ENAVAIL";
+		case 120: return "EISNAM";
+		case 121: return "EREMOTEIO";
+		default: return "(unknown)";
+	}
 }
 
 void DNotified(uint16_t q, uint16_t buf, size_t len, void *tag) {
