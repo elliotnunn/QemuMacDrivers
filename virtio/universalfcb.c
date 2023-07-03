0770051204fb4d76377bfbe94e977c01f15aaf2c
Elliot Nunn <elliotnunn@me.com>
2023-07-03 19:03:57 +0800

    universalfcb.c

---
+++ virtio/universalfcb.c
@@ -0,0 +1,35 @@
+#include <FSM.h>
+#include <MixedMode.h>
+
+#include "universalfcb.h"
+
+// Is the Mac OS 9 FCB format in use?
+// This can change at early boot, and is cheap to determine anyhow.
+static int fcbFormat9(void) {
+	short fcbLen = *(short *)0x3f6;
+	return fcbLen > 0 && fcbLen != 94;
+}
+
+static void *fcbBase(void) {
+	unsigned short hi = *(unsigned short *)0x34e;
+	unsigned short lo = *(unsigned short *)0x350;
+	return (void *)(((unsigned long)hi << 16) | lo);
+}
+
+OSErr UnivAllocateFCB(short *fileRefNum, FCBRecPtr *fileCtrlBlockPtr) {
+	if (fcbFormat9()) {
+		return CallUniversalProc(*(UniversalProcPtr *)0xe90, 0xfe8,
+			fileRefNum, fileCtrlBlockPtr);
+	} else {
+		void *base = fcbBase;
+		short len = *(short *)fcbBase;
+		for (short refnum=2; refnum<len; refnum+=94) {
+			if (*(long *)(base + refnum) == 0) {
+				*fileRefNum = refnum;
+				*fileCtrlBlockPtr = base + refnum;
+				return noErr;
+			}
+		}
+		return tmfoErr;
+	}
+}
---
+++ virtio/universalfcb.h
@@ -0,0 +1,10 @@
+// File System Manager functions (UT_*) to access the FCB table may be:
+// unavailable and unnecessary (-7.1)
+// available but unnecessary (7.5-8.6)
+// available and necessary (9.0-)
+// And the accessors weren't in InterfaceLib until 8.5!
+// So we must polyfill them.
+
+#pragma once
+
+OSErr UnivAllocateFCB(short *fileRefNum, FCBRecPtr *fileCtrlBlockPtr);
