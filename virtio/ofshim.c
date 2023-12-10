c1382a75ea0b1184b3081b32dfa6a6d50277f4e9
Elliot Nunn <elliotnunn@me.com>
2023-12-10 09:02:24 +0800

    Barely working ofshim with powerpc-eabi-gcc

---
+++ virtio/ofshim.c
@@ -0,0 +1,80 @@
+// Write Hot Code And Lie
+
+#include <stdarg.h>
+#include <stddef.h>
+
+// Open Firmware Client Interface, a special function pointer
+int (*ofci)(long *ary);
+
+// Protos
+int of(const char *s, int nargs, ...);
+
+// Until the linker works, this must be the first entry
+void openFirmwareEntry(void *initrd, long initrdsize, void *ci) {
+	ofci = ci;
+
+	int r;
+
+ 	r = ofci((long []){(long)"interpret", 1, 1, (long)".\" elliot wins\" cr", 0, 9999});
+
+	r = of("interpret",
+		1, "cr .\" how about this\" cr",
+		1, NULL);
+
+// 	if (r != 0) for (;;) of("abc", 0, 0);
+
+// 	const char *x = "interpret";
+// 	const char *y = "cr";
+//
+// 	long a[] = {(long)x, 1, 1, (long)y, 0, 99};
+// 	for (int i=0; i<10; i++) ofci(a);
+
+// 	if (a[3] == 99) return;
+// 	for (;;) ofci(a);
+
+// 	of("cr", 0, 0);
+}
+
+// Call wrapper for OF Client Interface
+// Call as of("name", nargs, arg1, arg2, nret, ret1, ret2)
+int of(const char *s, int nargs, ...) {
+	long array[16] = {(long)s, nargs, 1234 /*nret placeholder*/};
+	va_list list;
+
+	va_start(list, nargs);
+
+	for (int i=0; i<nargs; i++) {
+		array[3+i] = (long)va_arg(list, void *);
+// 		ofci((long []){(long)"interpret", 1, 1, (long)".\" narg cnt\" cr", 0, 9999});
+	}
+
+	int nrets = va_arg(list, int);
+
+// 	for (int i=0; i<nrets; i++) ofci((long []){(long)"interpret", 1, 1, (long)".\" nret cnt\" cr", 0, 9999});
+
+	array[2] = nrets;
+
+// 	for (int i=0; i<array[1]; i++)
+// 		ofci((long []){(long)"interpret", 1, 1, (long)".\" array1\" cr", 0, 9999});
+// 	for (int i=0; i<array[2]; i++)
+// 		ofci((long []){(long)"interpret", 1, 1, (long)".\" array2\" cr", 0, 9999});
+
+	ofci(array);
+
+// 	for (int i=0; i<nrets; i++) {
+// 		long *ptr = va_arg(list, long *);
+// 		if (ptr) *ptr = (long)array[3+nargs+i];
+// 	}
+
+// 	return retval;
+}
+
+void *memset(void *s, int c, long len) {
+    unsigned char *dst = s;
+    while (len > 0) {
+        *dst = (unsigned char) c;
+        dst++;
+        len--;
+    }
+    return s;
+}
